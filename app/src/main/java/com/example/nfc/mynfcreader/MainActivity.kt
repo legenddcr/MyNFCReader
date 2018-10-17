c0b8379736de5a42c3a42d955dd64567d13d62b7
Chengrui Deng <chengrui.dcr@alibaba-inc.com>
2018-10-17 11:29:06 +0800

    fix kt code issue and enable the NFC hello world read info demo

--- app/src/main/java/com/example/nfc/mynfcreader/MainActivity.kt
+++ app/src/main/java/com/example/nfc/mynfcreader/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.example.nfc.mynfcreader
 
 import android.app.Activity
-import android.os.Bundle
-import android.nfc.tech.MifareUltralight
-import android.nfc.tech.MifareClassic
-import android.content.Intent
 import android.app.PendingIntent
-import android.widget.Toast
+import android.content.Intent
 import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.MifareClassic
+import android.nfc.tech.MifareUltralight
+import android.os.Bundle
+import android.provider.Settings.ACTION_NFC_SETTINGS
 import android.view.View
 import android.widget.TextView
-import android.nfc.Tag
+import android.widget.Toast
 import com.example.nfc.mynfcreader.utils.Utils
+import com.example.nfc.mynfcreader.parser.NdefMessageParser
+import android.nfc.NdefMessage
+import android.nfc.NdefRecord
+import android.os.Parcelable
+import android.util.Log;
+
+
 
 
 class MainActivity : Activity() {
-    private var nfcAdapter:NfcAdapter? = null
+    private var nfcAdapter: NfcAdapter? = null
     // launch our application when a new Tag or Card will be scanned
     private var pendingIntent: PendingIntent? = null
     // display the data read
@@ -36,6 +44,29 @@ class MainActivity : Activity() {
         pendingIntent = PendingIntent.getActivity(this, 0,
                 Intent(this, this.javaClass)
                         .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val nfcAdapterRefCopy = nfcAdapter
+        if (nfcAdapterRefCopy != null) {
+            if (!nfcAdapterRefCopy.isEnabled())
+                showNFCSettings()
+
+            nfcAdapterRefCopy.enableForegroundDispatch(this, pendingIntent, null, null)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        setIntent(intent)
+        resolveIntent(intent)
+    }
+
+    private fun showNFCSettings() {
+        Toast.makeText(this, "You need to enable NFC", Toast.LENGTH_SHORT).show()
+        val intent = Intent(ACTION_NFC_SETTINGS)
+        startActivity(intent)
     }
 
     /**
@@ -107,5 +138,48 @@ class MainActivity : Activity() {
         }
 
         return sb.toString()
+    }
+
+    private fun resolveIntent(intent: Intent) {
+        val action = intent.action
+
+        if (NfcAdapter.ACTION_TAG_DISCOVERED == action
+                || NfcAdapter.ACTION_TECH_DISCOVERED == action
+                || NfcAdapter.ACTION_NDEF_DISCOVERED == action) {
+            val rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+
+            if (rawMsgs != null) {
+                Log.i("NFC", "Size:" + rawMsgs.size);
+                val ndefMessages: Array<NdefMessage> = Array(rawMsgs.size, {i -> rawMsgs[i] as NdefMessage});
+                displayNfcMessages(ndefMessages)
+            } else {
+                val empty = ByteArray(0)
+                val id = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID)
+                val tag = intent.getParcelableExtra<Parcelable>(NfcAdapter.EXTRA_TAG) as Tag
+                val payload = dumpTagData(tag).toByteArray()
+                val record = NdefRecord(NdefRecord.TNF_UNKNOWN, empty, id, payload)
+                val emptyMsg = NdefMessage(arrayOf(record))
+                val emptyNdefMessages: Array<NdefMessage> = arrayOf(emptyMsg);
+                displayNfcMessages(emptyNdefMessages)
+            }
+        }
+    }
+
+
+    private fun displayNfcMessages(msgs: Array<NdefMessage>?) {
+        if (msgs == null || msgs.isEmpty())
+            return
+
+        val builder = StringBuilder()
+        val records = NdefMessageParser.parse(msgs[0])
+        val size = records.size
+
+        for (i in 0 until size) {
+            val record = records[i]
+            val str = record.str()
+            builder.append(str).append("\n")
+        }
+
+        text?.setText(builder.toString())
     }
 }
